a67a68be34dc89abf2160172c5833b439ab37b74
olema <matushkin.oleg@gmail.com>
2016-01-01 06:15:27 +0300

    Добавлен pinger.py

---
+++ pinger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*-coding: utf-8 -*-
+
+# Скрипт пингует ресурсы в сети и отправляет e-mail
+
+import sys
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
