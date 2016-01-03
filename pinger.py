cceb0615e1a7e134abb3b2e3210a4f51fb1e45b6
olema <matushkin.oleg@gmail.com>
2016-01-03 05:58:02 +0300

    Добавлен код

--- pinger.py
+++ pinger.py
@@ -3,11 +3,30 @@
 
 # Скрипт пингует ресурсы в сети и отправляет e-mail
 
-import sys
 
+import sys
+import logging
+import subprocess
+
+
+logging.basicConfig(format='%(asctime)s %(message)s', 
+                            filename='logpinger.log', 
+                            level=logging.DEBUG)
+
+resources = (('Google DNS', '8.8.8.8'),
+             ('ya.ru', '213.180.193.3')
+,)
+def pinging(res):
+   command = ['ping', '-c2', '-W1', res[1]]
+   return subprocess.call(command)
 
 def main():
-    pass
+    logging.info('pinger started...')
+    for r in resources:
+        print(r)
+        logging.info('pinging {} ({}) .. - {}'.format(r[0], 
+                       r[1],
+                       'Ok' if not pinging(r) else 'not available..'))
 
 if __name__ == '__main__':
     main()
