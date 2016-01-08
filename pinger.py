6d40c23dfd2d98e0ac0c70235b460cc7aafc046a
olema <matushkin.oleg@gmail.com>
2016-01-08 15:14:51 +0300

    Глобальные переменные убраны в main(). В send_mail() добавлены параметры вызова

--- pinger.py
+++ pinger.py
@@ -11,44 +11,39 @@ import smtplib
 import time
 import platform
 
-logname = 'logpinger.log'
-
-fromaddr = 'ollo@netbook.home'
-toaddr = 'okibkursk-it@yandex.ru'
-subject = 'Ping results: ' + time.strftime('%a, %d %b %Y %H:%M:%S')
-
-logging.basicConfig(format='%(asctime)s %(message)s', 
-                            filename=logname, 
-                            level=logging.DEBUG)
-
-resources = (('localhost', '127.0.0.1'),
-             ('Google DNS', '8.8.8.8'),
-             ('ya.ru', '213.180.193.3'),
-             ('nothing host', '192.168.0.5')
-,)
-
-def mail_send(message):
+def mail_send(fromaddr, toaddr, subject, message):
     msg = 'From: {}\r\nTo: {}\r\nSubject: {}\r\n'.format(fromaddr, toaddr, subject)
     msg += message
     server = smtplib.SMTP('localhost')
     server.sendmail(fromaddr, toaddr, msg)
     server.quit()
     print(msg)
-    logging.info('message sended: ' + msg)
 
 def pinging(res):
    command = ['ping', '-c2', '-W1', res[1]]
    return subprocess.call(command)
 
 def main():
+    fromaddr = 'ollo@netbook.home'
+    toaddr = 'okibkursk-it@yandex.ru'
+    subject = 'Ping results: ' + time.strftime('%a, %d %b %Y %H:%M:%S')
+    logname = 'logpinger.log'
+    logging.basicConfig(format='%(asctime)s %(message)s', 
+                                filename=logname, 
+                                level=logging.DEBUG)
     logging.info('pinger started...')
+    resources = (('localhost', '127.0.0.1'),
+                 ('Google DNS', '8.8.8.8'),
+                 ('ya.ru', '213.180.193.3'),
+                 ('nothing host', '192.168.0.5')
+                ,)
     message = platform.platform() + '\n'
     for r in resources:
         print(r)
         pingresult = 'Ok' if pinging(r) == 0 else 'not avail'
-        logging.info('{} ({}) -- {}'.format(r[0], r[1], pingresult))
-        message += '{} ({}) -- {}\n'.format(r[0], r[1], pingresult)
-    mail_send(message)
+        logging.info('{:<20} ({:<15}) -- {}'.format(r[0], r[1], pingresult))
+        message += '{:<20} ({:<15}) -- {}\n'.format(r[0], r[1], pingresult)
+    mail_send(fromaddr, toaddr, subject, message)
 
 if __name__ == '__main__':
     main()
